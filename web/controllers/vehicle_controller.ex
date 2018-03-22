d49f8d4170a552e8f5385930db98708c19d02c3b
Melissa <melissa160@gmail.com>
2018-03-22 14:48:22 -0500

    setting index action in vehicle controller

--- web/controllers/vehicle_controller.ex
+++ web/controllers/vehicle_controller.ex
@@ -3,5 +3,18 @@ defmodule Registering.VehicleController do
 
   alias Registering.Vehicle
 
+  plug :find_user
   plug :action
+
+  defp find_user(conn, _) do
+    driver = Repo.get(Registering.Driver, conn.params["driver_id"])
+    assign(conn, :driver, driver)
+  end
+
+  def index(conn, _params) do
+    driver = conn.assigns.driver
+    vehicles = Repo.all assoc(driver, :vehicles)
+
+    render conn, vehicles: vehicles, driver: driver
+  end
 end
